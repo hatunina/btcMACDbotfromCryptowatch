de44940882af32cc034a7b611e3d253a531ce055
hatunina <y.iwama0807@gmail.com>
2018-09-06 23:19:39 +0900

    refactor

--- main.py
+++ main.py
@@ -17,31 +17,16 @@ if __name__ == '__main__':
     # 設定ファイルを読み込む
     config_dict = util.read_config(abs_config_path)
 
-    # 出力先ログファイルのsrcディレからの相対パス
-    log_file_relative_path = config_dict['log_file_relative_path']
-
-    # CryptowatchAPIのOHLC取得用パラメータ, 85400は日足
-    periods = config_dict['periods']
-
-    # 描画する日数
-    target_days_range = config_dict['target_days_range']
-
-    # Slackのtoken
-    token = config_dict['token']
-
-    # 投稿するチャンネル名
-    channel = config_dict['channel']
-
     # logファイルの相対パスを絶対パスに変換
-    abs_log_file_path = util.relative_to_abs(log_file_relative_path)
+    abs_log_file_path = util.relative_to_abs(config_dict['log_file_relative_path'])
 
     # logger初期化
     logger = Logger(abs_log_file_path).get_main_logger()
 
-    btcmacd = BtcMACD(logger, periods, target_days_range)
+    btcmacd = BtcMACD(logger, config_dict['periods'], config_dict['target_days_range'])
     message, abs_figure_path = btcmacd.pipeline()
 
-    slack = Slack(logger, token, channel)
+    slack = Slack(logger, config_dict['token'], config_dict['channel'])
     slack.notify_with_figure(message, abs_figure_path)
 
     logger.info('DONE')
